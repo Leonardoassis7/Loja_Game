16ea479532c5f8d5274c99bc1235f04927ccaeea
Leonardo Assis <leonardorobertoassis@gmail.com>
2023-02-01 17:24:28 -0300

    lojagame

---
+++ src/test/java/org/lojagame/game_eumsegredo/GameEUmSegredoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lojagame.game_eumsegredo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameEUmSegredoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
