9071b797891b29b6122eb0a8b6cc0ebfb22d6181
Leonardo Assis <leonardorobertoassis@gmail.com>
2023-02-07 17:08:26 -0300

    game

--- src/main/java/org/lojagame/game_eumsegredo/model/Categoria.java
+++ src/main/java/org/lojagame/game_eumsegredo/model/Categoria.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -32,6 +33,18 @@ public class Categoria {
 	@OneToMany(mappedBy="categoria", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("categoria")
 	private List<Produto> produto;
+
+	@ManyToOne
+    @JsonIgnoreProperties("categoria")
+    private Usuario usuario;
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	public Long getId() {
 		return id;
